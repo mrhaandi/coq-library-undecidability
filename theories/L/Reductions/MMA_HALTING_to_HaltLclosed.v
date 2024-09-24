e8032cce3af6642772b2f64773aa5cd4e53f3c1f
Andrej Dudenhefner <mrhaandi@gmail.com>
2024-09-24 13:09:39 +0200

    closed steps_apps_lams

--- theories/L/Reductions/MMA_HALTING_to_HaltLclosed.v
+++ theories/L/Reductions/MMA_HALTING_to_HaltLclosed.v
@@ -20,8 +20,6 @@ Context {num_regs : nat}. (* number of registers - 1 *)
 Notation N := (S num_regs).
 Context (P : list (mm_instr (Fin.t N))). (* program *)
 
-
-
 (* function composition *)
 Definition funcomp {X Y Z} (g : Y -> Z) (f : X -> Y) :=
   fun x => g (f x).
@@ -91,11 +89,11 @@ Proof.
 *)
 Admitted.
 
-Fixpoint substs ts t :=
+Fixpoint substs k ts t :=
   match t with
-  | var x => nth x ts (var x)
-  | app s t => app (substs ts s) (substs ts t)
-  | lam s => lam (substs (var 0 :: ts) s)
+  | var x => nth x (map var (seq 0 k) ++ ts) (var x)
+  | app s t => app (substs k ts s) (substs k ts t)
+  | lam s => lam (substs (S k) ts s)
   end.
 
 Fixpoint reds s ts :=
@@ -109,9 +107,16 @@ Fixpoint reds s ts :=
     end
   end.
 
-Lemma substs_nil t : substs [] t = t.
+Lemma substs_nil k t : substs k [] t = t.
 Proof.
-Admitted.
+  elim: t k.
+  - move=> x k /=. rewrite app_nil_r map_nth.
+    have [?|?] : x < k \/ x >= k by lia.
+    + by rewrite seq_nth.
+    + by rewrite nth_overflow ?length_seq.
+  - move=> ? IH1 ? IH2 ? /=. by rewrite IH1 IH2.
+  - move=> ? IH ? /=. by rewrite IH.
+Qed.
 
 Require Undecidability.L.Util.L_facts.
 Import L_facts (step).
@@ -130,51 +135,6 @@ Proof.
   by move=> /L_facts.eval_iff [_].
 Qed.
 
-(*
-Lemma subst_substs ts s t k : L_facts.bound k s ->
-  subst (substs ts s) k t = substs (fold_left (fun t's t => ) ts []) map (fun u => subst u k t) (combine (seq 0 ts) s.
-Proof.
-  move=> H. elim: H t ts.
-  - move=> {}k n ? t ts /=.
-    have E: var n = (fun u => subst u k t) (var n).
-    { rewrite /=. admit. }
-    by rewrite E map_nth -E.
-  - move=> {}k s1 s2 ? IH1 ? IH2 t ts /=.
-    by rewrite IH1 IH2.
-  - move=> {}k {}s ? IH t ts /=.
-    rewrite IH /=.
-    Search (nth _ (map _ _)).
-*)
-(*
-Lemma subst_substs ts s t k : L_facts.bound k s ->
-  subst (substs ts s) k t = substs (fold_left (fun t's t => ) ts []) map (fun u => subst u k t) (combine (seq 0 ts) s.
-Proof.
-  move=> H. elim: H t ts.
-  - move=> {}k n ? t ts /=.
-    have E: var n = (fun u => subst u k t) (var n).
-    { rewrite /=. admit. }
-    by rewrite E map_nth -E.
-  - move=> {}k s1 s2 ? IH1 ? IH2 t ts /=.
-    by rewrite IH1 IH2.
-  - move=> {}k {}s ? IH t ts /=.
-    rewrite IH /=.
-    Search (nth _ (map _ _)).
-
-Lemma subst_substs ts s t k : L_facts.bound k s ->
-  subst (substs ts s) k t = substs (map (fun u => subst u k t) (combine (seq 0 ts) s.
-Proof.
-  move=> H. elim: H t ts.
-  - move=> {}k n ? t ts /=.
-    have E: var n = (fun u => subst u k t) (var n).
-    { rewrite /=. admit. }
-    by rewrite E map_nth -E.
-  - move=> {}k s1 s2 ? IH1 ? IH2 t ts /=.
-    by rewrite IH1 IH2.
-  - move=> {}k {}s ? IH t ts /=.
-    rewrite IH /=.
-    Search (nth _ (map _ _)).
-*)
-
 Lemma subst_lams n s k t : subst (lams n s) k t = lams n (subst s (n + k) t).
 Proof.
   elim: n k; first done.
@@ -188,7 +148,7 @@ Proof.
   by apply: L_facts.star_trans_l.
 Qed.
 
-Lemma steps_apps_lams (ts : list term) s :
+Lemma steps_apps_lams_reds (ts : list term) s :
   Forall (fun t' => eval t' t') ts ->
   Forall closed ts ->
   ARS.star step (apps (lams (length ts) s) ts) (reds (lams (length ts) s) ts).
@@ -204,6 +164,60 @@ Proof.
     + by apply: L_facts.stepApp.
     + rewrite subst_lams Nat.add_0_r.
       by apply: ARS.starR.
+Qed.
+
+Lemma substs_closed k ts t : closed t -> substs k ts t = t.
+Proof.
+  move=> /L_facts.closed_dcl.
+  have : 0 <= k by lia.
+  move: (0)=> n + H. elim: H k.
+  - move=> > ? > ? /=. rewrite app_nth1.
+    + rewrite length_map length_seq. lia.
+    + by rewrite map_nth seq_nth; [lia|].
+  - move=> > ? IH1 ? IH2 * /=.
+    by rewrite IH1 ?IH2.
+  - move=> > ? IH * /=. by rewrite IH; [lia|].
+Qed.
+
+Lemma substs_subst k t ts s : closed t -> Forall closed ts ->
+  substs k ts (subst s (k + length ts) t) = substs k (ts ++ [t]) s.
+Proof.
+  move=> Ht Hts. elim: s k.
+  - move=> x k /=.
+    move E: (Nat.eqb x (k + length ts)) => [|].
+    + move=> /Nat.eqb_eq in E. subst.
+      rewrite app_assoc app_nth2 !length_app !length_map !length_seq; [lia|].
+      rewrite Nat.sub_diag /=. by apply: substs_closed.
+    + move=> /Nat.eqb_neq in E.
+      rewrite /= !app_assoc.
+      have [?|?] : x < k + length ts \/ x > k + length ts by lia.
+      * rewrite (app_nth1 _ [t]); last done.
+        by rewrite length_app length_map length_seq.
+      * rewrite !nth_overflow; last done.
+        all: rewrite !length_app length_map length_seq /=; lia.
+  - move=> ? IH1 ? IH2 ? /=. by rewrite IH1 IH2.
+  - move=> ? IH ? /=. by rewrite IH.
+Qed.
+
+Lemma steps_apps_lams (ts : list term) s :
+  Forall (fun t' => eval t' t') ts ->
+  Forall closed ts ->
+  ARS.star step (apps (lams (length ts) s) ts) (substs 0 (rev ts) s).
+Proof.
+  move=> H. elim: H s.
+  - move=> ? /= *. rewrite substs_nil. by apply ARS.starR.
+  - move=> t' {}ts H1t' Hts IH s /Forall_cons_iff [H2t'] /[dup] ? /IH {}IH.
+    move: H1t' => /L_facts.eval_iff [_] [t'' ?]. subst.
+    rewrite /=. apply: ARS.star_trans.
+    + apply: steps_apps_l.
+      apply: ARS.starC.
+      * by apply: L_facts.stepApp.
+      * rewrite subst_lams.
+        by apply: ARS.starR.
+    + apply: ARS.star_trans; first by apply IH.
+      rewrite Nat.add_0_r -length_rev.
+      rewrite substs_subst; [done|by apply: Forall_rev|].
+      by apply ARS.starR.
 Qed.
 
 Print Assumptions steps_apps_lams.
@@ -275,14 +289,40 @@ Fixpoint vec_seq i n : Vector.t nat n :=
 
 Definition enum_regs : Vector.t term N := Vector.map var (Vector.rev (vec_seq 0 N)).
 
+Lemma eval_enc_regs ts : eval (enc_regs ts) (enc_regs ts).
+Proof.
+  by constructor.
+Qed.
+
+Definition enc_nth (x : Fin.t N) : term :=
+  let n := proj1_sig (Fin.to_nat x) in
+  (* \cs. cs (\c1 .. cN.cx) *)
+  lam (app (var 0) (lams N (var (N - 1 - n)))).
+
+Lemma eval_lams_N s : eval (lams N s) (lams N s).
+Proof.
+  constructor.
+Qed.
+
+Opaque Nat.iter.
+
+Lemma enc_nth_spec x v : eval (app (enc_nth x) (enc_regs v)) (nat_enc (vec.vec_pos v x)).
+Proof.
+  unfold enc_nth. econstructor; [constructor|apply: eval_enc_regs|].
+  move: (Fin.to_nat x) => [n Hn].
+  rewrite [in proj1_sig _]/=.
+  move E: (N - 1 - n)=> m /=.
+  rewrite subst_lams /=.
+  have /Nat.eqb_neq -> : m <> S (num_regs + 0) by lia.
+  rewrite /enc_regs. econstructor; [constructor|apply: eval_lams_N|].
+  rewrite subst_apps /=.
+
+
 Definition enc_inc (x : Fin.t N) : term :=
   let n := proj1_sig (Fin.to_nat x) in
   lam (app (var 0) (lams N (app (lam (lam (apps (var 0) (map var ((seq 2 n) ++ [1] ++ seq (n+2) (N-n-2)))))) (app nat_succ (var n))))).
 
-Lemma eval_enc_regs ts : eval (enc_regs ts) (enc_regs ts).
-Proof.
-  constructor.
-Qed.
+
 
 
 
@@ -290,14 +330,16 @@ Lemma subst_ren n t k s : closed s -> subst (ren (fun x => n + x) t) (n + k) s =
 Proof.
 Admitted.
 
-Lemma eval_lams_N s : eval (lams N s) (lams N s).
-Proof.
-  constructor.
-Qed.
+
 
 Lemma subst_var_eq x s : subst (var x) x s = s.
 Proof.
   by rewrite /= Nat.eqb_refl.
+Qed.
+
+Lemma subst_var_neq x y s : x <> y -> subst (var x) y s = var x.
+Proof.
+  by move=> /= /Nat.eqb_neq ->.
 Qed.
 
 Lemma nat_enc_closed n : closed (nat_enc n).
@@ -329,22 +371,19 @@ Admitted.
 
 Opaque enc_inc.
 
-Definition enc_instr (instr : mm_instr (Fin.t N)) : term :=
+Definition enc_instr i (instr : mm_instr (Fin.t N)) : term :=
   match instr with
   | MM.mm_inc x =>
-      lam (lam (apps (lam (lam (enc_pair (var 2) (var 1)))) [app pi_succ (var 1); app (enc_inc x) (var 0)]))
-        (*
-      
-      
-      app (var 0) (
-        lams N (enc_pair
-          (app pi_succ (var (S N)))
-          (enc_regs (Vector.replace enum_regs x (app nat_succ (Vector.nth enum_regs x))))))))*)
-  | MM.mm_dec x k => var 0
+      (* \cs.(\cs'.\f.f (pi (S i)) cs') (inc x cs) *)
+      lam (app (lam (lam (apps (var 0) [pi (addr (S i)); var 1]))) (app (enc_inc x) (var 0)))
+  | MM.mm_dec x j =>
+      (* \cs.cs (\c1..cN.cx (\f. f (pi (S i)) cs) (\c.\f. f (pi j) (replace x cs c))) *)
+  
+  var 0
   end.
 
 Definition enc_state (p : mm_state N) : term :=
-  enc_pair (pi (addr (fst p))) (enc_regs (snd p)).
+  lam (apps (var 0) [pi (addr (fst p)); (enc_regs (snd p))]).
 
 Lemma eval_enc_state p : eval (enc_state p) (enc_state p).
 Proof.
@@ -359,8 +398,6 @@ Qed.
 Lemma closed_pi i : closed (pi (addr i)).
 Proof.
 Admitted.
-
-
 
 (*
 Lemma eval_apps_lams {n : nat} (ts : Vector.t term n) s t u :
@@ -448,23 +485,16 @@ Opaque enc_pair pi_succ pi enc_regs.
 
 Axiom FF : False.
 
-(* enc_instr at certain position? *)
-
 Lemma mma_step_sim (instr : mm_instr (Fin.t N)) (p q : mm_state N) :
-  mma_sss instr p q -> eval (apps (enc_instr instr) [pi (addr (fst p)); enc_regs (snd p)]) (enc_state q).
+  mma_sss instr p q -> eval (app (enc_instr (fst p) instr) (enc_regs (snd p))) (enc_state q).
 Proof.
   case.
   - (* INC *)
     move=> i x v /=.
-    econstructor; [|apply: eval_enc_regs|].
-    + econstructor; [constructor|apply: eval_pi|].
-      rewrite subst_lam. by constructor.
-    + rewrite /= !subst_enc_pair /= !enc_inc_closed.
-      econstructor; [|apply: enc_inc_spec|].
-      * econstructor; [constructor| |by rewrite /=; constructor].
-        rewrite !pi_succ_closed !pi_addr_closed. by apply: pi_succ_spec.
-      * rewrite !subst_enc_pair /= !pi_addr_closed.
-        rewrite /enc_state /=. apply: eval_enc_pair.
+    econstructor; [by constructor|by apply: eval_enc_regs|].
+    rewrite /= pi_addr_closed enc_inc_closed.
+    econstructor; [by constructor|by apply: enc_inc_spec|].
+    rewrite /= pi_addr_closed. by constructor.
   - (* DEC *)
     destruct FF.
   - (* JUMP *)
@@ -482,6 +512,9 @@ Definition enc_run := lam (apps (var 0) [lam (lam (apps enc_step [var 1; var 0])
 Lemma enc_run_spec (p q : mm_state N) : @sss_step _ _ (@mma_sss N) (1, P) p q ->
   ARS.star L_facts.step (apps enc_run [enc_state p; enc_run]) (apps enc_run [enc_state q; enc_run]).
 Proof.
+  move=> [k [l [instr [r [cs]]]]].
+  move=> [[??]] [?]. subst k p.
+  Check sss_step.
 Admitted.
 
 Lemma enc_halt_spec (p : mm_state N) : fst p < 1 \/ S (length P) <= fst p ->
@@ -495,8 +528,27 @@ Proof.
 Admitted.
 
 
+Lemma closed_enc_run : closed enc_run.
+Proof.
+Admitted.
+
+Lemma closed_enc_state p : closed (enc_state p).
+Proof.
+Admitted.
+
+Lemma transport p q : sss_output (@mma_sss N) (1, P) p q ->
+  eval (app (app enc_run (enc_state p)) enc_run) (enc_regs (snd q)).
+Proof.
+Admitted.
+
+Lemma reflection p cs : eval (app (app enc_run (enc_state p)) enc_run) (enc_regs cs) ->
+  exists k, sss_steps (@mma_sss N) (1, P) k p (0, cs).
+Proof.
+Admitted.
+
 End MMA_HALTING_to_HaltLclosed.
 
+Opaque enc_run enc_state.
 
 Require Import Undecidability.Synthetic.Definitions.
 
@@ -507,25 +559,34 @@ Lemma reduction n : @MMA_HALTING (S n) ⪯ HaltLclosed.
 Proof.
   unshelve eexists.
   - intros [P v]. exists (apps (enc_run P) [enc_state P (1, v); enc_run P]).
-    admit.
+    intros u k. cbn. by rewrite !closed_enc_run closed_enc_state.
   - intros [P v]. split.
-    + intros [q Hq]. exists (nat_enc (Vector.hd (snd q))). cbn. admit. (* forwards simulation *)
-    + intros [t Ht]. cbn in Ht. assert (H't := enc_run_spec' Ht).
-      destruct H't as [cs ?]. subst.
+    + intros [q Hq]. exists (enc_regs (snd q)). cbn in *.
+      now apply transport. (* simulation *)
+    + intros [t Ht]. cbn in Ht.
+      assert (H't := enc_run_spec' Ht). destruct H't as [cs ?]. subst.
       exists (0, cs). split.
-      * admit. (* backwards simulation *)
+      * now apply reflection. (* inverse simulation *)
       * cbn. lia.
-Admitted.
+Qed.
 
+Print Assumptions reduction.
 
 Theorem MMA_computable_to_L_computable_closed {k} (R : Vector.t nat k -> nat -> Prop) :
   MMA_computable R -> L_computable_closed R.
 Proof.
   unfold MMA_computable, L_computable_closed.
   move=> [n [P HP]].
-  exists (apps (enc_run P) [(var 0); enc_run P]). (* TODO actual wrapper *)
+  (* \c1...\cn. run (\f.f pi_1 (\g.g 0 c1 .. ck 0 .. 0)) run (\c'1...\c'n.c'1)) *)
+  pose t_state := lam (apps (var 0) [pi P (addr P 1);
+    lam (apps (var 0) ([nat_enc 0] ++ map var (rev (seq 1 k)) ++ (repeat (nat_enc 0) n)))]).
+  exists (lams (1 + k + n) (apps (enc_run P) [t_state; enc_run P; lams (1 + k + n) (var (k + n))])).
+  assert (H_t_state : closed t_state).
+  { admit. }
   split.
-  - admit. (* closedness *)
+  - intros u ?.
+    rewrite subst_lams subst_apps !map_cons !closed_enc_run subst_lams H_t_state.
+    now rewrite subst_var_neq; [lia|].
   - move=> v. split.
     + move=> m. rewrite HP. split.
       * intros [c [v' [H1 H2]]]. admit. (* forwards simulation *)
